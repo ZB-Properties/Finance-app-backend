644074a5c6dc243531b20067ed29191bcb10a595
Shope Alli-B <zoeyalli921@gmail.com>
2025-05-21 11:26:23 -0700

    Initial backend commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,5 +6,10 @@ const { signup, login } = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/login', login);
+// in authRoutes.js
+router.get('/test', (req, res) => {
+  res.json({ success: true });
+});
+
 
 module.exports = router;
