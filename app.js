1623ccf7f5d7626e2fbfcd6d737f0a39a26a1445
Shope Alli-B <zoeyalli921@gmail.com>
2025-05-21 12:24:21 -0700

    initial backend commit

--- app.js
+++ app.js
@@ -11,14 +11,12 @@ const path = require('path');
 const app = express();
 
 
-
 app.use(cors({
   origin: 'https://financetracks-app.netlify.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
 
 
 app.use(express.json());
