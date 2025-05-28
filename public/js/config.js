d46dd0933caab9ebe9d4580bb91f1801edeb1d39
Shope Alli-B <zoeyalli921@gmail.com>
2025-05-27 17:47:31 -0700

    Initial backend commit

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 
 export const API_BASE_URL = window.location.hostname === 'localhost'
   ? 'http://localhost:2600/api'
-  : 'https://postgres-production-3629.up.railway.app/api';
+  : 'http://finance-app-backend-production-30b1.up.railway.app/api';
 
   console.log('Using API_BASE_URL', API_BASE_URL);
